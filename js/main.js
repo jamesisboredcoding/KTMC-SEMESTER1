3063883cf522cef70651505795c17bffe1394bfa
unknown <aividas.silingas08@gmail.com>
2026-01-05 22:19:07 +0200

    created and styled html

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
+import { NotifyInfo } from "./modules/notifications.js"
+import { get_config } from "./modules/utils.js"
+import { Section } from "./modules/category_section.js"
+
+// Elements
+
+// Functions
+
+get_config().then(response => NotifyInfo(response))
+
+// Init
+
+const popular = new Section("In Theatres", [])
+popular.render(document.querySelector(".category-sections"))
+
+// Window DOM events
+
 window.addEventListener("scroll", () => {
     document.querySelector(".header").classList.toggle("scrolled", scrollY > 0)
 })
